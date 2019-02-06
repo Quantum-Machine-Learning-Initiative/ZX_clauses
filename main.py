8875f91bed1e0ebf36e5ca66e33d98713ef77659
Alexey Uvarov <alexejuv@yandex.ru>
2019-02-06 12:52:35 +0300

    Declared more functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def random_clause(num_variables, k):
     '''Generate a random clause for k-SAT. Returns a list of three
     integers
@@ -13,7 +15,33 @@ def random_SAT_instance(num_variables, k, num_clauses):
     # that's for later
     return [random_clause(num_variables, k) for i in num_clauses]
 
+def make_SAT_Hamiltonian(sat_instance, pauli):
+    '''Creates a Hamiltonian from a SAT instance. pauli specifies which
+    basis to use
+    returns a sparse matrix
+    '''
+    pass
 
+def find_energy_gap(H):
+    '''finds the energy gap of a given Hamiltonian, i.e. the difference
+    between the ground state and the first excited state
+    '''
+    pass
 
+def average_energy_gap(num_variables_1, num_clauses_1, k_1,
+                       num_variables_2, num_clauses_2, k_2, num_instances=50):
+    '''
+    Calculate the average energy gap for the ZX clause Hamiltonians
+    '''
+    dE_data = []
+    for i in num_instances:
+        instance_1 = random_SAT_instance(num_variables_1, k_1, num_clauses_1)
+        instance_2 = random_SAT_instance(num_variables_2, k_2, num_clauses_2)
+        H = make_SAT_Hamiltonian(instance_1, 'Z') + make_SAT_Hamiltonian(instance_2, 'X')
+        dE = find_energy_gap(H)
+        dE_data.append(dE)
+    return np.mean(dE_data)
+
+    
 if (__name__=='__main__'):
     pass
